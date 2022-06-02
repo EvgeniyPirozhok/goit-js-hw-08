d8246a8b5383ffd5331cd8063ad70e686651a4da
EvgeniyPirozhok <miloigrovoe@gmail.com>
2022-06-02 23:42:55 +0300

    task 3 допилить нужно

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle";
+const refs = {
+    form: document.querySelector('.feedback-form'),
+}
+const form = document.querySelector('form.feedback-form');
+const emaiIinput = document.querySelector('input[name="email"]');
+const textarea = document.querySelector('textarea[name="message"]');
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+
+form.addEventListener('input', throttle(onFormText, 500));
+form.addEventListener('submit', onFormSubmit);
+onPlaceTextForm();
+
+function onFormText(evt) {
+    const formData = { email: emaiIinput.value, message: textarea.value, }
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    
+};
+
+function onPlaceTextForm() {
+    const savedTextForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    
+    if (savedTextForm) {
+        emaiIinput.value = savedTextForm.email || "";
+        textarea.value = savedTextForm.message || "";
+    }
+
+};
